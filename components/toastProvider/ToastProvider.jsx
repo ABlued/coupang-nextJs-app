334dc7cbde8bf24e8a2d3d27d670a482c37623f2
ABlued <skwodhkswkd@naver.com>
2023-10-21 23:38:14 +0900

    feat : inject ToastProvider

---
+++ components/toastProvider/ToastProvider.jsx
@@ -0,0 +1,9 @@
+'use client';
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+function ToastProvider() {
+  return <ToastContainer autoClose={2000}></ToastContainer>;
+}
+
+export default ToastProvider;
