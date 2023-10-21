89c028b642ec211df37709160a9be2458f8b7a3b
ABlued <skwodhkswkd@naver.com>
2023-10-21 19:45:47 +0900

    feat : implement ResetPage

---
+++ src/app/(auth)/reset/ResetClient.jsx
@@ -0,0 +1,56 @@
+'use client';
+import Input from '@/components/Input/Input';
+import Button from '@/components/button/Button';
+import Heading from '@/components/heading/Heading';
+import Loader from '@/components/loader/Loader';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import styles from './Reset.module.scss';
+function ResetClient() {
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const resetPassword = () => {
+    e.preventDefault();
+    setIsLoading(true);
+  };
+  return (
+    <>
+      {isLoading && <Loader />}
+
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <div className={styles.form}>
+            <Heading
+              title="비밀번호 업데이트"
+              subtitle="비밀번호를 업데이트 하세요"
+            />
+            <form onSubmit={resetPassword}>
+              <Input
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={styles.control}
+              />
+              <div>
+                <Button type="submit">업데이트</Button>
+              </div>
+              <div className={styles.links}>
+                <p>
+                  <Link href="/login">로그인 </Link>
+                </p>
+                <p>
+                  <Link href="/register">회원가입 </Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default ResetClient;
