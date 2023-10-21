7b245c2e2858249b185074a22fdcc8839e2fbf4b
ABlued <skwodhkswkd@naver.com>
2023-10-21 17:41:37 +0900

    feat : implement Button Component

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import classNames from 'classnames';
+import React from 'react';
+import styles from './Button.module.scss';
+function Button({
+  type = 'button',
+  secondary = false,
+  bgColor,
+  fgColor,
+  width,
+  ...restProps
+}) {
+  const composeClasses = classNames(
+    styles.button,
+    secondary ? styles.secondary : styles.primary
+  );
 
-function Button() {
+  const style = {
+    backgroundColor: bgColor || '',
+    color: fgColor || '',
+    width: width || '',
+  };
+
   return (
-    <div>Button</div>
-  )
+    <button
+      type={type}
+      className={composeClasses}
+      style={style}
+      {...restProps}
+    ></button>
+  );
 }
 
-export default Button
+export default Button;
--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -9,6 +9,8 @@ import Loader from '@/components/loader/Loader';
 import Input from '@/components/Input/Input';
 import AutoSignInCheckbox from '@/components/autoSignInCheckbox/AutoSignInCheckbox';
 import Divider from '@/components/divider/Divider';
+import Button from '@/components/button/Button';
+import Link from 'next/link';
 const LoginClient = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -65,10 +67,17 @@ const LoginClient = () => {
               />
             </div>
             <div className={styles.buttonGroup}>
+              <Button type="submit" width="100%">
+                로그인
+              </Button>
               <Divider />
+              <Button width="100%" secondary>
+                <Link href={'/register'}>회원가입</Link>
+              </Button>
               <Divider />
-              Button
-              <div>Button</div>
+              <div>
+                <Button onclick={signInWithGoogle}>구글 로그인 </Button>
+              </div>
             </div>
           </form>
         </div>
