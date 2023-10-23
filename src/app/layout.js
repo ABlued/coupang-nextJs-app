faeff628628e7dc41641836f733c29684cca7b56
ABlued <skwodhkswkd@naver.com>
2023-10-23 23:37:30 +0900

    chore : redux store setting

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import Footer from '@/layouts/footer/Footer';
 import Header from '@/layouts/header/Header';
+import ReduxProviders from '@/redux/provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +19,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <ToastProvider>{children}</ToastProvider>
-        <Footer />
+        <ReduxProviders>
+          <Header />
+          <ToastProvider>{children}</ToastProvider>
+          <Footer />
+        </ReduxProviders>
       </body>
     </html>
   );
