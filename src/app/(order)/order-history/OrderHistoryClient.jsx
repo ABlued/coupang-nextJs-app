5179dbf81815e26c666967a9da3b670f285b0941
ABlued <skwodhkswkd@naver.com>
2023-12-31 21:22:45 +0900

    feat : implement OrderHistory UI

--- src/app/(order)/order-history/OrderHistoryClient.jsx
+++ src/app/(order)/order-history/OrderHistoryClient.jsx
@@ -5,6 +5,11 @@ import useFetchCollection from '@/src/hooks/useFetchCollection';
 import { useDispatch, useSelector } from 'react-redux';
 import { STORE_ORDERS, selectOrderHistory } from '@/src/redux/slice/orderSlice';
 import { selectUserId } from '@/src/redux/slice/authSlice';
+import Heading from '@/src/components/heading/Heading';
+import Loader from '@/src/components/loader/Loader';
+import { formatTime } from '@/src/utils/dayjs';
+import { priceFormat } from '@/src/utils/priceFormat';
+import { useRouter } from 'next/navigation';
 function OrderHistoryClient() {
   const { data, isLoading } = useFetchCollection('orders');
   const dispatch = useDispatch();
@@ -14,7 +19,61 @@ function OrderHistoryClient() {
 
   const orders = useSelector(selectOrderHistory);
   const userID = useSelector(selectUserId);
-  return <div>OrderHistoryClient</div>;
+  const filteredOrders = orders.filter((order) => order.userID === userID);
+  const router = useRouter();
+  const handleClick = (id) => {
+    router.push(`/order-details/${id}`);
+  };
+
+  return (
+    <section className={styles.order}>
+      <Heading title="주문 목록">
+        {isLoading && <Loader />}
+        <div className={styles.table}>
+          {!filteredOrders.length ? (
+            <p>주문 목록이 없습니다.</p>
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  <th>순서</th>
+                  <th>주문 날짜</th>
+                  <th>주문 아이디</th>
+                  <th>주문 금액</th>
+                  <th>주문 상태</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredOrders.map((order, index) => {
+                  const { id, orderDate, orderTime, orderAmount, orderStatus } =
+                    order;
+                  return (
+                    <tr key={id} onClick={() => handleClick(id)}>
+                      <td>{index + 1}</td>
+                      <td>{formatTime(orderDate)}</td>
+                      <td>{id}</td>
+                      <td>{priceFormat(orderAmount)}원</td>
+                      <td>
+                        <p
+                          className={
+                            orderStatus !== '배송완료'
+                              ? `${styles.pending}`
+                              : `${styles.delivered}`
+                          }
+                        >
+                          {orderStatus}
+                        </p>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </Heading>
+    </section>
+  );
 }
 
 export default OrderHistoryClient;
